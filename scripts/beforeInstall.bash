7541f6a4d63e186387b3d6789ba20620a6d751b0
tejaslohar <108939755+tejaslohar@users.noreply.github.com>
2023-07-10 23:12:29 +0530

    Update beforeInstall.bash

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -1,1 +1,8 @@
-ls
+#!/usr/bin/env bash
+ # I want to make sure that the directory is clean and has nothing left over from
+ # previous deployments. The servers auto scale so the directory may or may not
+ # exist.
+ if [[ -d /home/ec2-user/project ]] ; then
+  rm -rf /home/ec2-user/project
+ fi
+ mkdir -vp /home/ec2-user/project
